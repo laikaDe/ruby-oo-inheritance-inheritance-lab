3455f2a9895249068ed01c18d17387678076e322
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-02-15 16:33:33 -0800

    commit tree 9b45c96e22b2032023b53f3a63e267a14e32650c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+#Student inherits from the user class
 
+class Student < User
+    
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
